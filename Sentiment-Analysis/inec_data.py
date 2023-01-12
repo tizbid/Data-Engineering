61a839b90018ee333398b5a1fd8bf007b191cb1e
Bede <56255193+tizbid@users.noreply.github.com>
2023-01-12 01:44:08 +0100

    Extract full tweets fixed

--- Sentiment-Analysis/inec_data.py
+++ Sentiment-Analysis/inec_data.py
@@ -22,40 +22,50 @@ def auth(config = configparser.ConfigParser()):
     return api
 
 
-api = auth()
 
+def get_tweets(topic:str, num_of_tweets,api = auth()):
+    # Get the 100 most recent tweets made to the handle
+    tweets = api.search_tweets(q=topic, tweet_mode='extended',count=num_of_tweets)
 
+    # Create an empty DataFrame to store the tweets
+    tweets_df = pd.DataFrame(columns=[ "text"])
 
+    # Clean tweet and print the text of each tweet
+    for tweet in tweets:
 
-# Replace "handle" with the actual handle you want to get tweets for
-handle = "inec"
+        # Ensure full text is returned
+        try:
+            tweet = tweet.retweeted_status.full_text 
+        except AttributeError:  # So this is not a Retweet
+            tweet = tweet.full_text
 
-# Get the 100 most recent tweets made to the handle
-tweets = api.search_tweets(q=handle, tweet_mode='extended',count=5)
+        # Remove hashtags, user mentions, and URLs
+        tweet = re.sub(r'#\w+', '', tweet)
+        tweet = re.sub(r'@\w+', '', tweet)
+        tweet = re.sub(r'https?:\/\/\S+', '', tweet)
 
-# Print the text of each tweet
-for tweet in tweets:
-    
-    #print(tweet.text)
-    
-    # Remove hashtags, user mentions, and URLs
-    tweet = re.sub(r'#\w+', '', tweet.full_text)
-    tweet = re.sub(r'@\w+', '', tweet)
-    tweet = re.sub(r'https?:\/\/\S+', '', tweet)
+        # Remove punctuation and convert to lowercase
+        #tweet = re.sub(r'[^\w\s]', '', tweet)
+        tweet = tweet.lower()
+
+        # Remove stopwords
+
+        stopwords_list = stopwords.words('english')
+        tweet_words = tweet.split()
+        tweet_without_stopwords = [word for word in tweet_words if word not in stopwords_list]
+
+        # Rejoin tweet
+        tweet = ' '.join(tweet_without_stopwords)
+
+        tweet_df = pd.DataFrame({ "text": [tweet]})
+
+        tweets_df = tweets_df.append(tweet_df, ignore_index=True)
         
-    # Remove punctuation and convert to lowercase
-    #tweet = re.sub(r'[^\w\s]', '', tweet)
-    tweet = tweet.lower()
-        
-    # Remove stopwords
-        
-    stopwords_list = stopwords.words('english')
-    tweet_words = tweet.split()
-    tweet_without_stopwords = [word for word in tweet_words if word not in stopwords_list]
-        
-    # Rejoin tweet
-    tweet = ' '.join(tweet_without_stopwords)
-     
-    
-    print(tweet)
+    tweets_df.to_csv('tweets_df.csv',index=False)
+    return 
+
+get_tweets('inec',10)
+
+
+
 
